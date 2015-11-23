8fe1710b4dcc9d5851465f63586d326437fd0aa8
Kenneth Lee <kennyxlee@gmail.com>
2015-11-23 11:36:39 -0800

    add name popups for sf state + cal

--- map.js
+++ map.js
@@ -7,13 +7,14 @@ var cal = L.circle([37.8700, -122.2590], 1000, {
   fillColor: '#F2A900',
   fillOpacity: 0.25
 }).addTo(map);
+cal.bindPopup("University of California, Berkeley");
 
-// Gators
 var sfState = L.circle([37.7233, -122.4797], 1000, {
   color: '#231161',
   fillColor: '#CD9700',
   fillOpacity: 0.25
 }).addTo(map);
+sfState.bindPopup("San Francisco State University");
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
